ffdab34061827b3b2d5d29cbfa169cab8670f19b
CristinaColimba <cris_c1004@hotmail.com>
2024-03-21 11:49:49 -0500

    update

---
+++ js/app.js
@@ -0,0 +1,10 @@
+UpperCamelCase, camelCase, snake_case
+let nombre = "Hola Mundo";
+let NombreCompleto;
+let nombreCompleto;
+
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = 'Colimba';
